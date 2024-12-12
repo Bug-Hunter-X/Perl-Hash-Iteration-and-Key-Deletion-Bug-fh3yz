a645685fb6234d41adb594654888d8e424745916
BugHunter-X <askzenai@gmail.com>
2024-12-12 15:30:05 +0100

    Add bug.pl file

---
+++ bug.pl
@@ -0,0 +1,12 @@
+my %hash = (
+    'a' => 1,
+    'b' => 2,
+    'c' => 3
+);
+
+foreach my $key (keys %hash) {
+    print "Key: $key, Value: $hash{$key}\n";
+}
+
+# Unexpected behavior when a key is deleted during iteration
+delete $hash{'b'};
